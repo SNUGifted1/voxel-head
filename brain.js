a33e6d93fd9c91562ffd260a63b59dae3d95a852
문태현 <snugifted6@gmail.com>
2017-08-14 17:00:52 +0900

    udp-server

--- brain.js
+++ brain.js
@@ -1,21 +1,17 @@
-var PORT = '5002';
-var HOST = '127.0.0.1';
+var dgram = require("dgram");
 
-var dgram = require('dgram');
-var server = dgram.createSocket('udp4');
+var server = dgram.createSocket("udp4");
 
-server.on('listening', function () {
-    var address = server.address();
-    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+server.on("message", function (msg, rinfo) {
+  console.log("server got: " + msg + " from " +
+    rinfo.address + ":" + rinfo.port);
 });
 
-server.on('message', function (message, remote) {
-    console.log(remote.address + ':' + remote.port +' - ' + message);
-    /*
-    if(message === 'true') {
-      return message;
-    }
-    */
+server.on("listening", function () {
+  var address = server.address();
+  console.log("server listening " +
+      address.address + ":" + address.port);
 });
 
-server.bind(PORT, HOST);
+server.bind(5004);
+// server listening 0.0.0.0:41234
